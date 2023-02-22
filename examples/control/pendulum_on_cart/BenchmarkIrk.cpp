4d23bf47393b0a601601f93be7785ea2601c39a0
Mikhail Katliar <cepstr@gmail.com>
2023-02-22 19:44:44 +0100

    Added IRK with sensitivities benchmark

---
+++ examples/control/pendulum_on_cart/BenchmarkIrk.cpp
@@ -0,0 +1,43 @@
+// Copyright 2023 Mikhail Katliar
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "CasadiImplicitDae.hpp"
+#include "CasadiImplicitDaeS.hpp"
+#include "ModelDimensions.hpp"
+
+#include <tmpc/integrator/ImplicitRungeKutta.hpp>
+#include <tmpc/integrator/GaussRadauIIAMethod.hpp>
+
+#include <benchmark/benchmark.h>
+
+
+static void BM_irkWithSensitivities(::benchmark::State& state)
+{
+    CasadiImplicitDae dae;
+    CasadiImplicitDaeS dae_s;
+    tmpc::ImplicitRungeKutta<double> irk {tmpc::GaussRadauIIAMethod {3}, NX, NZ, NU};
+
+    blaze::StaticVector<double, NX> const x0 {5.863919e-02, 3.176782e+00, -2.557677e-01, 3.518985e+00};
+    blaze::StaticMatrix<double, NX, NX + NU> Sx(0.);
+    diagonal(Sx) = 1.;
+    blaze::StaticVector<double, NU> u(0.);
+    blaze::StaticVector<double, NX> xf;
+    blaze::StaticMatrix<double, NX, NX + NU> Sxf;
+
+    for (auto _ : state)
+        irk(dae, dae_s, 0., 0.01, x0, Sx, u, xf, Sxf);
+}
+
+BENCHMARK(BM_irkWithSensitivities);
+
