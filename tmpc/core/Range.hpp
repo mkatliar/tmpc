de06b92e6513c80351ebaac20e13198f3af51053
Mikhail Katliar <cepstr@gmail.com>
2023-05-20 15:57:14 +0300

    Using std::views instead of Boost views

--- tmpc/core/Range.hpp
+++ tmpc/core/Range.hpp
@@ -1,34 +1,9 @@
 #pragma once
 
-#include <boost/range/irange.hpp>
-#include <boost/range/iterator_range.hpp>
-#include <boost/range/adaptor/indexed.hpp>
-#include <boost/range/adaptor/reversed.hpp>
-#include <boost/range/adaptor/transformed.hpp>
+#include <ranges>
 
 
-namespace tmpc :: views
+namespace tmpc
 {
-    // Make it look like std::views
-
-    template <typename Integer>
-    inline auto iota(Integer first, Integer last)
-    {
-        return boost::irange(first, last);
-    }
-
-
-    template <typename Range>
-    inline auto all(Range& range)
-    {
-        return boost::make_iterator_range(range);
-    }
-
-
-    // using boost::make_iterator_range;
-    // using boost::adaptors::indexed;
-    auto const reverse = boost::adaptors::reversed;
-    // using boost::adaptors::reverse;
-    auto const transform = boost::adaptors::transformed;
-    // using boost::adaptors::transformed;
+    namespace views = std::views;
 }
